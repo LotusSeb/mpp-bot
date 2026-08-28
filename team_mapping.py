bc6442ecdeb8e425a6e7d7701c8e04fa9430e782
LotusSeb <sebsdp@yahoo.fr>
2026-08-28 07:25:36 +0200

    Add files via upload

---
+++ team_mapping.py
@@ -0,0 +1,24 @@
+"""
+Dictionnaire de correspondance: noms MPP -> IDs API football-data.org
+"""
+
+MPP_TO_API_ID = {
+    "LOSC": 521,
+    "Paris SG": 524,
+    "Strasbourg": 576,
+    "RC Lens": 546,
+    "AJ Auxerre": 519,
+    "Angers SCO": 532,
+    "Brest": 512,
+    "Toulouse FC": 511,
+    "FC Lorient": 525,
+    "Troyes": 531,
+    "OL": 523,
+    "Havre AC": 533,
+    "Paris FC": 1045,
+    "OGC Nice": 522,
+    "Rennes": 529,
+    "Le Mans": 535,
+    "AS Monaco": 548,
+    "OM": 516,
+}
